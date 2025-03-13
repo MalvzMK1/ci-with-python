d428b894efb9531f43aeeb831241f7d6a896c537
Matheus Oliveira <matheus.oliveira@institutojef.org.br>
2025-03-13 14:29:10 -0300

    test: add more tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,4 @@
+import pytest
 from ci_with_python import User
 
 def test_user() -> None:
@@ -7,6 +8,28 @@ def test_user() -> None:
   assert user.age is 19
 
   user.name = 'João'
-
+ 
   assert user.name is 'João'
   assert user.age is 19
+
+# Expect a failure
+@pytest.mark.xfail
+def test_divide_by_zero():
+  assert 1 / 0 is 1
+
+# Skip test
+@pytest.mark.skip(reason="Not implemented yet")
+def test_user_field_validations():
+  with pytest.raises(Exception) as e_info:
+    User('', -1)
+
+# Test with multiple parameters
+@pytest.mark.parametrize("test_input,expected", [
+  ({'name': 'Matheus', 'age': 19}, 19),
+  ({'name': 'João', 'age': 38}, 38),
+  ({'name': 'Ana', 'age': 19}, 19),
+  ({'name': 'Leonardo', 'age': 20}, 20),
+  ({'name': 'Alexandre', 'age': 39}, 39),
+])
+def test_multiple_user_creations(test_input, expected):
+  assert User(test_input['name'], test_input['age']).age is expected
