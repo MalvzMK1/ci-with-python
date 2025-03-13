dc6ba4602587eefcd755c12b7ec96734bb3a5b4e
Matheus Oliveira <matheus.oliveira@institutojef.org.br>
2025-03-13 14:50:23 -0300

    fix: lint files

--- tests/test_course_class.py
+++ tests/test_course_class.py
@@ -1,19 +1,20 @@
 from ci_with_python import CourseClass, User
 
+
 def test_course_class() -> None:
-  teacher = User('Caio', 32)
-  course_class = CourseClass(teacher)
+    teacher = User('Caio', 32)
+    course_class = CourseClass(teacher)
 
-  assert course_class.teacher is teacher
-  assert course_class.students.__len__() is 0
+    assert course_class.teacher == teacher
+    assert course_class.students.__len__() == 0
 
-  student = User('Matheus', 19)
+    student = User('Matheus', 19)
 
-  course_class.add_student(student)
+    course_class.add_student(student)
 
-  assert course_class.students.__len__() is 1
-  assert course_class.students[0] is student
+    assert course_class.students.__len__() == 1
+    assert course_class.students[0] == student
 
-  course_class.remove_student_by_idx(0)
+    course_class.remove_student_by_idx(0)
 
-  assert course_class.students.__len__() is 0
+    assert course_class.students.__len__() == 0
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,35 +1,39 @@
 import pytest
 from ci_with_python import User
 
+
 def test_user() -> None:
-  user = User('Matheus', 19)
+    user = User('Matheus', 19)
 
-  assert user.name is 'Matheus'
-  assert user.age is 19
+    assert user.name == 'Matheus'
+    assert user.age == 19
 
-  user.name = 'João'
- 
-  assert user.name is 'João'
-  assert user.age is 19
+    user.name = 'João'
+
+    assert user.name == 'João'
+    assert user.age == 19
+
 
 # Expect a failure
 @pytest.mark.xfail
 def test_divide_by_zero():
-  assert 1 / 0 is 1
+    assert 1 / 0 == 1
+
 
 # Skip test
 @pytest.mark.skip(reason="Not implemented yet")
 def test_user_field_validations():
-  with pytest.raises(Exception) as e_info:
-    User('', -1)
+    with pytest.raises(Exception):
+        User('', -1)
+
 
 # Test with multiple parameters
 @pytest.mark.parametrize("test_input,expected", [
-  ({'name': 'Matheus', 'age': 19}, 19),
-  ({'name': 'João', 'age': 38}, 38),
-  ({'name': 'Ana', 'age': 19}, 19),
-  ({'name': 'Leonardo', 'age': 20}, 20),
-  ({'name': 'Alexandre', 'age': 39}, 39),
+    ({'name': 'Matheus', 'age': 19}, 19),
+    ({'name': 'João', 'age': 38}, 38),
+    ({'name': 'Ana', 'age': 19}, 19),
+    ({'name': 'Leonardo', 'age': 20}, 20),
+    ({'name': 'Alexandre', 'age': 39}, 39),
 ])
 def test_multiple_user_creations(test_input, expected):
-  assert User(test_input['name'], test_input['age']).age is expected
+    assert User(test_input['name'], test_input['age']).age == expected
